8c354a03c8cfddc4d30190298118ef3651f1c063
Nicholas Majeske <nmajeske@login26.chn>
2023-01-23 22:09:16 -0800

    First push of new MessagePassing class for batched node features, edges, and edge weights.

---
+++ nn/conv/message_passing.py
@@ -0,0 +1,232 @@
+import torch
+import torch_geometric
+from torch_sparse import SparseTensor
+
+from torch import Tensor
+from inspect import Parameter
+from torch_scatter import gather_csr, scatter, segment_csr, scatter_sum
+from torch_scatter.utils import broadcast
+from torch_sparse import SparseTensor, fill_diag, matmul, mul
+from torch_sparse import sum as sparsesum
+
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.nn.dense.linear import Linear
+from torch_geometric.nn.inits import zeros
+from torch_geometric.typing import Adj, OptTensor, PairTensor, Optional
+#from torch_geometric.utils import add_remaining_self_loops
+from torch_geometric.utils.num_nodes import maybe_num_nodes
+
+
+class MessagePassing(torch_geometric.nn.conv.MessagePassing):
+
+    def __check_input__(self, edge_index, size):
+        the_size: List[Optional[int]] = [None, None]
+
+        if isinstance(edge_index, Tensor):
+            assert edge_index.dtype == torch.long
+#            assert edge_index.dim() == 2
+            assert edge_index.size(-2) == 2
+            if size is not None:
+                the_size[0] = size[0]
+                the_size[1] = size[1]
+            return the_size
+
+        elif isinstance(edge_index, SparseTensor):
+            if self.flow == 'target_to_source':
+                raise ValueError(
+                    ('Flow direction "target_to_source" is invalid for '
+                     'message propagation via `torch_sparse.SparseTensor`. If '
+                     'you really want to make use of a reverse message '
+                     'passing flow, pass in the transposed sparse tensor to '
+                     'the message passing module, e.g., `adj_t.t()`.'))
+            the_size[0] = edge_index.sparse_size(1)
+            the_size[1] = edge_index.sparse_size(0)
+            return the_size
+
+        raise ValueError(
+            ('`MessagePassing.propagate` only supports `torch.LongTensor` of '
+             'shape `[2, num_messages]` or `torch_sparse.SparseTensor` for '
+             'argument `edge_index`.'))
+
+    def __lift__(self, src, edge_index, dim):
+        if isinstance(edge_index, Tensor):
+            index = torch.squeeze(
+                torch.index_select(edge_index, -2, torch.tensor([dim], device=edge_index.device)), 
+                -2
+            )
+            index = align_dims(
+                index, 
+                src, 
+                -1, 
+                self.node_dim, 
+            )
+            return torch.take_along_dim(src, index, self.node_dim)
+        elif isinstance(edge_index, SparseTensor):
+            if dim == 1:
+                rowptr = edge_index.storage.rowptr()
+                rowptr = expand_left(rowptr, dim=self.node_dim, dims=src.dim())
+                return gather_csr(src, rowptr)
+            elif dim == 0:
+                col = edge_index.storage.col()
+                return src.index_select(self.node_dim, col)
+        raise ValueError
+        
+    def __collect__(self, args, edge_index, size, kwargs):
+        i, j = (1, 0) if self.flow == 'source_to_target' else (0, 1)
+
+        out = {}
+        for arg in args:
+            if arg[-2:] not in ['_i', '_j']:
+                out[arg] = kwargs.get(arg, Parameter.empty)
+            else:
+                dim = 0 if arg[-2:] == '_j' else 1
+                data = kwargs.get(arg[:-2], Parameter.empty)
+
+                if isinstance(data, (tuple, list)):
+                    assert len(data) == 2
+                    if isinstance(data[1 - dim], Tensor):
+                        self.__set_size__(size, 1 - dim, data[1 - dim])
+                    data = data[dim]
+
+                if isinstance(data, Tensor):
+                    self.__set_size__(size, dim, data)
+                    data = self.__lift__(data, edge_index,
+                                         j if arg[-2:] == '_j' else i)
+
+                out[arg] = data
+
+        if isinstance(edge_index, Tensor):
+            out['adj_t'] = None
+            out['edge_index'] = edge_index
+            out['edge_index_i'] = torch.squeeze(
+                torch.index_select(edge_index, -2, torch.tensor([i], device=edge_index.device)), 
+                -2
+            )
+            out['edge_index_j'] = torch.squeeze(
+                torch.index_select(edge_index, -2, torch.tensor([j], device=edge_index.device)), 
+                -2
+            )
+            out['ptr'] = None
+        elif isinstance(edge_index, SparseTensor):
+            out['adj_t'] = edge_index
+            out['edge_index'] = None
+            out['edge_index_i'] = edge_index.storage.row()
+            out['edge_index_j'] = edge_index.storage.col()
+            out['ptr'] = edge_index.storage.rowptr()
+            out['edge_weight'] = edge_index.storage.value()
+            out['edge_attr'] = edge_index.storage.value()
+            out['edge_type'] = edge_index.storage.value()
+
+        out['index'] = out['edge_index_i']
+        out['size'] = size
+        out['size_i'] = size[1] or size[0]
+        out['size_j'] = size[0] or size[1]
+        out['dim_size'] = out['size_i']
+
+        return out
+
+    def aggregate(self, inputs: Tensor, index: Tensor, ptr: Optional[Tensor] = None, dim_size: Optional[int] = None) -> Tensor:
+        r"""Aggregates messages from neighbors as
+        :math:`\square_{j \in \mathcal{N}(i)}`.
+
+        Takes in the output of message computation as first argument and any
+        argument which was initially passed to :meth:`propagate`.
+
+        By default, this function will delegate its call to scatter functions
+        that support "add", "mean" and "max" operations as specified in
+        :meth:`__init__` by the :obj:`aggr` argument.
+        """
+        if ptr is not None:
+            ptr = expand_left(ptr, dim=self.node_dim, dims=inputs.dim())
+            return segment_csr(inputs, ptr, reduce=self.aggr)
+        else:
+            return scatter(inputs, index, dim=self.node_dim, dim_size=dim_size, reduce=self.aggr)
+
+
+def align_dims(x, y, x_dim, y_dim):
+    if len(x.shape) > len(y.shape):
+        raise ValueError(
+            "Input x must have fewer dimensions than y to be aligned. Received x.shape=%s and y.shape=%s" % (
+                x.shape, y.shape
+            )
+        )
+    elif len(x.shape) == len(y.shape):
+        return x
+    if x_dim < 0:
+        x_dim = len(x.shape) + x_dim
+    if y_dim < 0:
+        y_dim = len(y.shape) + y_dim
+    new_shape = [1 for _ in y.shape]
+    start = y_dim - x_dim
+    end = start + len(x.shape)
+    new_shape[start:end] = x.shape
+    return x.view(new_shape)
+
+
+def align(inputs, dims):
+    if not (isinstance(inputs, tuple) or isinstance(inputs, list)) or not all([isinstance(inp, torch.Tensor) for inp in inputs]):
+        raise ValueError("Argumet inputs must be tuple or list of tensors")
+    if len(inputs) < 2:
+        return inputs
+    if isinstance(dims, int):
+        dims = tuple(dims for inp in inputs)
+    elif not isinstance(dim, tuple):
+        raise ValueError("Argument dim must be int or tuple of ints")
+    input_dims = [inp.dim() for inp in inputs]
+    idx = input_dims.index(max(input_dims))
+    y = inputs[idx]
+    y_dim = dims[idx]
+    return [align_dims(inp, y, dim, y_dim) for inp, dim in zip(inputs, dims)]
+
+
+def add_remaining_self_loops(edge_index, edge_weight: Optional[torch.Tensor] = None, fill_value: float = 1., num_nodes: Optional[int] = None):
+    r"""Adds remaining self-loop :math:`(i,i) \in \mathcal{E}` to every node
+    :math:`i \in \mathcal{V}` in the graph given by :attr:`edge_index`.
+    In case the graph is weighted and already contains a few self-loops, only
+    non-existent self-loops will be added with edge weights denoted by
+    :obj:`fill_value`.
+
+    Args:
+        edge_index (LongTensor): The edge indices.
+        edge_weight (Tensor, optional): One-dimensional edge weights.
+            (default: :obj:`None`)
+        fill_value (float, optional): If :obj:`edge_weight` is not :obj:`None`,
+            will add self-loops with edge weights of :obj:`fill_value` to the
+            graph. (default: :obj:`1.`)
+        num_nodes (int, optional): The number of nodes, *i.e.*
+            :obj:`max_val + 1` of :attr:`edge_index`. (default: :obj:`None`)
+
+    :rtype: (:class:`LongTensor`, :class:`Tensor`)
+    """
+    if edge_index.dim() > 2:
+        orig_size = edge_index.size()
+        n_edges = orig_size[-1]
+        edge_index = edge_index.view(-1, 2, n_edges)
+        edge_index_set, edge_weight_set = [], []
+        for i in range(edge_index.size(0)):
+            _edge_index = edge_index[i]
+            _edge_weight = edge_weight
+            if not edge_weight is None:
+                _edge_weight = edge_weight[i]
+            _edge_index, _edge_weight = torch_geometric.utils.add_remaining_self_loops(
+                _edge_index, _edge_weight, fill_value, num_nodes
+            )
+            edge_index_set.append(_edge_index)
+            edge_weight_set.append(_edge_weight)
+        try:
+            edge_index = torch.cat(edge_index_set)
+        except RuntimeError as e:
+            if "Sizes of tensors must match" in str(e):
+                raise RuntimeError(
+                    (
+                        "Failed to add self loops to dynamic edge_index because number "
+                        "of added self loops was not uniform across instances resulting "
+                        "in a ragged tensor."
+                    )
+                )
+        edge_index = edge_index.view(orig_size[:-1] + edge_index.size()[-1:])
+        if not edge_weight is None:
+            edge_weight = torch.cat(edge_weight_set, 0)
+            edge_weight = edge_weight.view(orig_size[:-1] + edge_weight.size()[-1:])
+        return edge_index, edge_weight
+    return torch_geometric.utils.add_remaining_self_loops(edge_index, edge_weight, fill_value, num_nodes)
